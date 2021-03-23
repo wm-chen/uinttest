61596a8ac092db1313b3a39cd7eb54d14865cc03
wm-chen <13052071102@163.com>
2021-03-23 17:39:54 +0800

    jenkins

---
+++ actions/product_action.py
@@ -0,0 +1,5 @@
+# encoding: utf-8
+# author: wm-chen
+# product_action.py
+# 2021/3/20 3:52 下午
+# desc:
---
+++ main.py
@@ -0,0 +1,11 @@
+# encoding: utf-8
+# author: wm-chen
+# main.py
+# 2021/3/23 5:38 下午
+# desc:
+
+from run.run_all_case import RunAllCases
+
+
+if __name__ == '__main__':
+    RunAllCases().run()
